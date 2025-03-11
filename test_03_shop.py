6fd7d73809f4ac49b6718e72a1c9420f492c2019
KseniaKlein1604 <kseniarayevskaya1999@gmail.com>
2025-03-11 10:07:42 +0300

    test_03_shop.py

--- test_03_shop.py
+++ test_03_shop.py
@@ -1,13 +1,24 @@
+import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.get("https://www.saucedemo.com/")
+def test_purchase_flow(driver):
+    wait = WebDriverWait(driver, 5)
+
+    driver.get("https://www.saucedemo.com/")
+    wait.until(EC.presence_of_element_located((By.ID, "user-name"))).send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+
+
 
 waiter = WebDriverWait(driver, 40)
 button_user = driver.find_element(By.ID, 'user-name').send_keys("standard_user")
@@ -29,12 +40,10 @@ button_zip_code = driver.find_element(By.ID, 'postal-code').send_keys("12345678"
 
 button_continue = driver.find_element(By.ID, 'continue').click()
 
-total=driver.find_element(By.CSS_SELECTOR, 'div.summary_total_label').text
-print(f'Total: {total}')
-clean_total = total.replace('Total: ', '').strip()
-assert clean_total == '$58.29'
-input("Press Enter to continue...")
+total_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "summary_total_label")))
+    total = total_element.text
 driver.quit()
+assert total == "Total: $58.29", f"Ожидаемая сумма: $58,29, получилось {total}"
 
 
 
